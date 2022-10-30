0107b9af5416715321a4f55b3136a1c63cc184d9
Bruno Melaneo <bruno.melaneo@volpi-group.com>
2022-10-30 13:18:18 +0100

    A few more tweaks... at least it compiles...

--- boards/metro_m4/examples/dac.rs
+++ boards/metro_m4/examples/dac.rs
@@ -21,11 +21,6 @@ use pac::{  interrupt,
             CorePeripherals,
             Peripherals,
             TC4,
-//            DAC_OTHER,      //  ToDo: fix "error[E0603]: function `DAC_OTHER` is private"
-        //    DAC_EMPTY_0,
-        //    DAC_EMPTY_1,
-        //    DAC_RESRDY_0,
-        //    DAC_RESRDY_1
         };
 
 
@@ -46,10 +41,10 @@ use hal::timer::TimerCounter;
 use cortex_m::peripheral::NVIC;
 
 use cortex_m::{interrupt::Mutex};
-
-static G_TIMER: Mutex<RefCell<Option<hal::timer::TimerCounter<TC4>>>> =
-    Mutex::new(RefCell::new(None));
-
+use crate::pac::gclk::genctrl::SRC_A::DFLL;
+use pac::gclk::pchctrl::GEN_A::GCLK0;   //  Cyclops has a centralized
+                                        //  point where all clock are setup...
+                                        //  Make sure the clock is correct...
 
 
 //static G_LED: Mutex<RefCell<Option<   hal::gpio::Pin<hal::gpio::PA16, hal::gpio::PushPullOutput>   >>> = Mutex::new(RefCell::new(None));
@@ -70,11 +65,17 @@ fn main() -> ! {
             &mut peripherals.NVMCTRL,
     );
 
+    //  ToDo:   Make sure the generic clock "GCLK_DAC" has been properly...
+    //let dac_clock =
+    //    gen_clcks.configure_gclk_divider_and_source(GCLK0,
+    //         1, DFLL, false);
+    //todo!();
+
+
     //  instantiate Timer 4
     let gclk0 = gen_clcks.gclk0();
     let tc4 = gen_clcks.tc4_tc5(&gclk0).unwrap();
     let mut timer_4 =
-        //TimerCounter4::tc4_(
         TimerCounter::<TC4>::tc4_(
             &tc4,                   //  &clock::Tc4Tc5Clock
             peripherals.TC4,        //  TC4
@@ -83,33 +84,32 @@ fn main() -> ! {
 
     //  Configure the interrupt controller
     unsafe {
-        core.NVIC.set_priority(interrupt::TC4, 1);
-        NVIC::unmask(interrupt::TC4);
+        core.NVIC.set_priority(interrupt::DAC_OTHER     , 1);
+        core.NVIC.set_priority(interrupt::DAC_EMPTY_0   , 1);
+        core.NVIC.set_priority(interrupt::DAC_EMPTY_1   , 1);
+        core.NVIC.set_priority(interrupt::DAC_RESRDY_0  , 1);
+        core.NVIC.set_priority(interrupt::DAC_RESRDY_1  , 1);
+
+        NVIC::unmask(interrupt::DAC_OTHER   );
+        NVIC::unmask(interrupt::DAC_EMPTY_0 );
+        NVIC::unmask(interrupt::DAC_EMPTY_1 );
+        NVIC::unmask(interrupt::DAC_RESRDY_0);
+        NVIC::unmask(interrupt::DAC_RESRDY_1);
     }
 
-    //  Start Timer...
-    timer_4.start(10.hz());
 
-    //  Enable interrupt...
-    timer_4.enable_interrupt();
-
-    //  Store timer in the GLOBAL TIMER so it can be
-    //  accessed from the interrupt ISR
-    cortex_m::interrupt::free(|cs| {
-        G_TIMER.borrow(cs).replace(Some(timer_4));
-    });
 
 
 
     //  Inidialise the dac..
-    let (dac, dac0, dac1) =
+    let (mut dac, dac0, _dac1) =
         Dac::init(   &mut peripherals.MCLK,
                     peripherals.DAC);
-    dac0.enable(&mut dac);
+
     //  Enable dac
-    dac.enable_dac_controller();
-    //  Chnage the dac DATA register to start a new conversion...
-  //  dac0.start_conversion(0);
+    dac = dac.enable_dac_controller();
+    //  Change the dac0 DATA register to start a new conversion...
+    dac0.start_conversion(&mut dac);
 
 
 
@@ -122,30 +122,23 @@ fn main() -> ! {
     // PA02 :: Dac Vout0: LED_T_CTRL_BF: Bright temperature control
     // PA05 :: Dac Vout1: LED_T_CTRL_DF: Darkfield temperature control
     let pins = bsp::Pins::new(peripherals.PORT);
-    let dac_0_out_pin = pins.a0.into_alternate::<B>();
-    let dac_1_out_pin = pins.a0.into_alternate::<B>();
+    let dac_0_out_pin =
+        pins.a0.into_alternate::<B>();
+    let dac_1_out_pin =
+        pins.a5.into_alternate::<B>();
 
 
 
 
-    //  Select and set the Pin we need...
-    let pins = bsp::Pins::new(peripherals.PORT);
-    let red_led = pins.d13.into_push_pull_output();
-
-    //  Store red_led in the GLOBAL LED so it can be
-    //  accessed from the interrupt ISR
-    cortex_m::interrupt::free(|cs| {
-        G_LED.borrow(cs).replace(Some(red_led));
-    });
-
-
-
-
-
-
-
-
-
+//    //  Select and set the Pin we need...
+//    let pins = bsp::Pins::new(peripherals.PORT);
+//    let red_led = pins.d13.into_push_pull_output();
+//
+//    //  Store red_led in the GLOBAL LED so it can be
+//    //  accessed from the interrupt ISR
+//    cortex_m::interrupt::free(|cs| {
+//        G_LED.borrow(cs).replace(Some(red_led));
+//    });
 
 
 
@@ -167,83 +160,81 @@ fn main() -> ! {
 
 
 #[interrupt]
-fn TC4() {
-    //  Give some feedback in the terminal
-    rprint!(" DAC_OTHER interrupt ");
-
-    /*  Execute closure in critical section...   */
-    cortex_m::interrupt::free(|cs| {
-
-
-    })
-}
-
-#[interrupt]
 fn DAC_OTHER() {
     //  Give some feedback in the terminal
     rprint!(" DAC_OTHER interrupt ");
 
-    /*  Execute closure in critical section...   */
-    cortex_m::interrupt::free(|cs| {
+    //  To do:  clear interrupt flag etc...
+    todo!();
 
-
-    })
+//    /*  Execute closure in critical section...   */
+//    cortex_m::interrupt::free(|cs| {
+//        disable interrupt flag...
+//    })
 }
 
 
 
-//#[interrupt]
-//fn DAC_EMPTY_0() {
-//    //  Give some feedback in the terminal
-//    rprint!(" DAC_EMPTY_0 interrupt ");
-//
+#[interrupt]
+fn DAC_EMPTY_0() {
+    //  Give some feedback in the terminal
+    rprint!(" DAC_EMPTY_0 interrupt ");
+
+    //  To do:  clear interrupt flag etc...
+    todo!();
+
 //    /*  Execute closure in critical section...   */
 //    cortex_m::interrupt::free(|cs| {
-//
-//
+//        disable interrupt flag...
 //    })
-//}
-//
-//
-//
-//#[interrupt]
-//fn DAC_EMPTY_1() {
-//    //  Give some feedback in the terminal
-//    rprint!(" DAC_EMPTY_1 interrupt ");
-//
+}
+
+
+
+#[interrupt]
+fn DAC_EMPTY_1() {
+    //  Give some feedback in the terminal
+    rprint!(" DAC_EMPTY_1 interrupt ");
+
+    //  To do:  clear interrupt flag etc...
+    todo!();
+
 //    /*  Execute closure in critical section...   */
 //    cortex_m::interrupt::free(|cs| {
-//
-//
+//        disable interrupt flag...
 //    })
-//}
-//
-//
-//
-//#[interrupt]
-//fn DAC_RESRDY_0() {
-//    //  Give some feedback in the terminal
-//    rprint!(" DAC_RESRDY_0 interrupt ");
-//
+}
+
+
+
+#[interrupt]
+fn DAC_RESRDY_0() {
+    //  Give some feedback in the terminal
+    rprint!(" DAC_RESRDY_0 interrupt ");
+
+    //  To do:  clear interrupt flag etc...
+    todo!();
+
 //    /*  Execute closure in critical section...   */
 //    cortex_m::interrupt::free(|cs| {
-//
-//
+//        disable interrupt flag...
 //    })
-//}
-//
-//
-//
-//#[interrupt]
-//fn DAC_RESRDY_1() {
-//    //  Give some feedback in the terminal
-//    rprint!(" DAC_RESRDY_1 interrupt ");
-//
+}
+
+
+
+#[interrupt]
+fn DAC_RESRDY_1() {
+    //  Give some feedback in the terminal
+    rprint!(" DAC_RESRDY_1 interrupt ");
+
+    //  To do:  clear interrupt flag etc...
+    todo!();
+
 //    /*  Execute closure in critical section...   */
 //    cortex_m::interrupt::free(|cs| {
-//
-//
+//        disable interrupt flag...
 //    })
-//}
+}
 
 
