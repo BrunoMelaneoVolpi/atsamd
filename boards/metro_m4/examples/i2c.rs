89f9e259e7d3f4ebcd17a1dbd4152e540d3a643a
Bruno Melaneo <bruno.melaneo@volpi-group.com>
2023-01-23 12:22:45 +0100

    Exercising the I2C in with the Adafruit I2C Pressure and Temperature sensor...

--- boards/metro_m4/examples/i2c.rs
+++ boards/metro_m4/examples/i2c.rs
@@ -29,8 +29,25 @@ use hal::delay::Delay;
 
 use rtt_target::{/*rprint,*/ rprintln, rtt_init_print};
 
-const LENGTH: usize = 1;
-const ADDRESS: u8 = 0x77;
+struct ReadWrite(u8);
+struct MemoryMap;
+#[allow(dead_code)]
+impl MemoryMap{
+    pub const PADC_MSB : ReadWrite = ReadWrite(0x00);   //  10-bit Pressure ADC output value MSB 8
+    pub const PADC_LSB : ReadWrite = ReadWrite(0x01);   //  10-bit Pressure ADC output value LSB 2
+    pub const TADC_MSB : ReadWrite = ReadWrite(0x02);   //  10-bit Temperature ADC output value MSB 8
+    pub const TACD_LSB : ReadWrite = ReadWrite(0x03);   //  10-bit Temperature ADC output value LSB 2
+    pub const A0_MSB   : ReadWrite = ReadWrite(0x04);   //  a0 coefficient MSB 8
+    pub const A0_LSB   : ReadWrite = ReadWrite(0x05);   //  a0 coefficient LSB 8
+    pub const B1_MSB   : ReadWrite = ReadWrite(0x06);   //  b1 coefficient MSB 8
+    pub const B1_LSB   : ReadWrite = ReadWrite(0x07);   //  b1 coefficient LSB 8
+    pub const B2_MSB   : ReadWrite = ReadWrite(0x08);   //  b2 coefficient MSB 8
+    pub const B2_LSB   : ReadWrite = ReadWrite(0x09);   //  b2 coefficient LSB 8
+    pub const C12_MSB  : ReadWrite = ReadWrite(0x0A);   //  c12 coefficient MSB 8
+    pub const C12_LSB  : ReadWrite = ReadWrite(0x0B);   //  c12 coefficient LSB
+    pub const CONVERT  : ReadWrite = ReadWrite(0x12);   //  Start Pressure and Temperature Conversion
+}
+
 
 #[entry]
 fn main() -> ! {
@@ -50,16 +67,16 @@ fn main() -> ! {
         &mut peripherals.NVMCTRL,
     );
 
-//    let mclk = peripherals.MCLK;
-    //let dmac = peripherals.DMAC;
+
     let pins = bsp::Pins::new(peripherals.PORT);
 
-    // Take SDA and SCL
-    let (sda, scl) = (pins.pb02, pins.scl);
+    //  Take SDA and SCL
+    let (sda,
+         scl,
+         shut_down
+         ) = (pins.pb02, pins.scl, pins.d13);
 
-
-//sda.into_pull_down_disabled();
-
+    /*  Setup I2C interface...  */
     let i2c_sercom = periph_alias!(peripherals.i2c_sercom);
     let mclk = &mut peripherals.MCLK;
     let mut i2c =
@@ -70,45 +87,113 @@ fn main() -> ! {
                         sda,
                         scl);
 
+    /*  Setup Shutdown pin...   */
+    let mut shut_down = shut_down.into_push_pull_output();
 
-
+    /*  Setup delay feature...      */
     let core = CorePeripherals::take().unwrap();
     let mut delay = Delay::new(core.SYST, &mut clocks);
 
+    /*  Reset device...         */
+    /*      Shut down...        */
+    shut_down.set_low().unwrap();
+    /*      Wait...             */
+    delay.delay_us(100u16);
+    /*      Revive the thing... */
+    shut_down.set_high().unwrap();
+    /*      Let it wake up...   */
+    delay.delay_us(100u16);
 
-    let mut buffer = [0x00; 1];
-//    panic!("asdfasdfasdfasdfasdfasdfasdfasdf");
+
+
+
+
     // Test writing then reading from an I2C chip
+    //let mut buffer = [0x00; 1];
+    //i2c.write_read(ADDRESS, &[0x00], &mut buffer).unwrap();
+
+
+    //  panic!("asdfasdfasdfasdfasdfasdfasdfasdf");
+    //  Test writing then reading from an I2C chip
     const ADDRESS: u8 = 0x60;
 
-    let x = i2c.write_read(ADDRESS, &[0x12], &mut buffer);
-    //delay.delay_us(1u16);
-    //rprintln!("===== i2c tx Rx ===========");
+    /*  Documentation is SSSHIIITTTT
+            Read Coeficients...
+                <-I2c Write->   <-I2c read->
+                [0xC0], [0x04], [0xC1], [0x3E], [0xCE], [0xB3], [0xF9], [0xC5], [0x17], [0x33], [0xC8]
+                addW    coef    addR    <--------------- Read Value ------------------------------->        */
+    const NUMBER_OF_COEFFICIENTS : usize = 8;
+
+    let mut buffer = [0x00; NUMBER_OF_COEFFICIENTS];
+    for coefficients in buffer{
+        rprintln!("     data {:#02x}", coefficients);
+    }
+
+    rprintln!("==== Reading coefficients ============");
+    let _x = i2c.write(ADDRESS, &[MemoryMap::A0_MSB.0]);
+
+    let _x = i2c.read(ADDRESS, &mut buffer);
+
+    for coefficients in buffer{
+        rprintln!("     data {:#02x}", coefficients);
+    }
+
+    rprintln!("================");
+    //  Start conversion...
+    let x = i2c.write(ADDRESS, &[MemoryMap::CONVERT.0, 00_u8]);      //  ok
     match x {
         Result::Ok(t) => {
             rprintln!("Ok({:?})", t)
         }
         Result::Err(e) => {
-            rprintln!("Err({:?})", e)
+            loop {
+                rprintln!("Err({:?})", e);
+            } //  Forever...
         }
     }
-    //x.unwrap();
 
+    delay.delay_us(1000u16);
 
-    let mut x = [0u8;1];
-    loop {
-        delay.delay_ms(10u16);
-
-        x[0] += 1;
-
-        let _result = i2c.write_read(ADDRESS, &x, &mut buffer);
-        rprintln!("===== {:?}", x[0]);
+    /*  Get Pressure...     */
+    rprintln!("==== Reading pressure ============");
+    const PRESSURE_TEMP_BYTES : usize = 2;
+    let mut pressure_temperature = [0x00; PRESSURE_TEMP_BYTES];
+    for press in pressure_temperature{
+        rprintln!("     press {:#02x}", press);
     }
 
-    rprintln!("===== i2c looping ===========");
+    rprintln!(" ------------------------------------ ");
+    let _x = i2c.write(ADDRESS, &[MemoryMap::PADC_MSB.0]);
+
+    let _x = i2c.read(ADDRESS, &mut pressure_temperature);
+
+    for press in pressure_temperature{
+        rprintln!("     press {:#02x}", press);
+    }
+
+
+    /*  Get Temperature...  */
+    rprintln!("==== Reading temperature ============");
+    pressure_temperature = [0x00; PRESSURE_TEMP_BYTES];
+    for temperature in pressure_temperature{
+        rprintln!("     temperature {:#02x}", temperature);
+    }
+
+    rprintln!(" ------------------------------------ ");
+    let _x = i2c.write(ADDRESS, &[MemoryMap::TADC_MSB.0]);
+
+    let _x = i2c.read(ADDRESS, &mut pressure_temperature);
+
+    for temperature in pressure_temperature{
+        rprintln!("     temperature {:#02x}", temperature);
+    }
+
+
 
     loop {
-        // Go to sleep
-        asm::wfi();
+        delay.delay_ms(1000u16);
+        rprintln!("===== DEMO OVER ====");
     }
+
+
 }
